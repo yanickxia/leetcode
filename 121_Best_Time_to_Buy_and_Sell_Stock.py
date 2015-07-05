c6411e8c9b9f0d304270ff32870e6be3e3b20004
Yann <me@yannxia.info>
2015-07-05 11:24:26 +0800

    add 121

--- 121_Best_Time_to_Buy_and_Sell_Stock.py
+++ 121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,1 +1,28 @@
 __author__ = 'Yann.Xia'
+
+
+class Solution:
+    # \@param {integer[]} prices
+    # @return {integer}
+    def maxProfit(self, prices):
+
+        if prices == []:
+            return 0
+
+        min_point = prices[0]
+        max_profit = 0
+        for i in range(0, len(prices)):
+            if min_point > prices[i]:
+                min_point = prices[i]
+
+            profit = prices[i] - min_point
+            if profit > max_profit:
+                max_profit = profit
+
+        return max_profit
+
+
+s = Solution()
+print(s.maxProfit([4, 1, 2]))
+print(s.maxProfit([2, 4, 1]))
+print(s.maxProfit([7, 2, 4, 1]))
