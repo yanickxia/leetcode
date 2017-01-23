3584a5df54927d5134ef1c30ec60f368766570ed
yann <me.yan.xia@qq.com>
2017-01-23 23:24:40 +0800

    Update

--- 10_Regular_Expression_Matching.py
+++ 10_Regular_Expression_Matching.py
@@ -1,40 +1,22 @@
 # -*- coding:utf-8 -*-
 from unittest import TestCase
 
-SPITE = "SPITE"
-MATCH = "MATCH"
-ANY = "ANY"
-
-
-class State:
-    def __init__(self):
-        self.c = ''
-        self.out = State
-
-
 class Solution(object):
     def __init__(self):
         self.root = None
 
-    def isMatch(self, s, p):
+    def isMatch(self, text, reg):
         """
-        :type s: str
-        :type p: str
+        :type text: str
+        :type reg: str
         :rtype: bool
         """
 
-    def gen_state(self, p):
-        i, p_size = 0, len(p)
-        current_state = None
-        while i < p_size:
-            if str(p[i]).isalpha():
-                if str(p[i + 1]).isalpha():
-                    current_state = State
-                    current_state.c = p[i]
-                    i += 1
-            if str(p[i + 1]) == '.':
-                if str(p[i + 1]) == '*':
-                    current_state.out =
+
+class DFA(object):
+    def compile(self, pattern):
+        
+
 
 
 
