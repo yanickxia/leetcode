8ab84b98af722a92fab86cbac932c75789d98a4b
yann <me.yan.xia@qq.com>
2017-02-14 18:16:11 +0800

    Add 14

--- 14_Longest_Common_Prefix.py
+++ 14_Longest_Common_Prefix.py
@@ -1,1 +1,26 @@
 # -*- coding:utf-8 -*-
+
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+
+        if len(strs) == 0:
+            return ""
+
+        row, column, row_size, column_size, comm_str = 0, 0, len(min(strs)), len(strs), ""
+
+        for row in xrange(0, row_size, 1):
+            for column in xrange(1, column_size, 1):
+                if strs[column - 1][row] != strs[column][row]:
+                    return comm_str
+            comm_str += strs[0][row]
+
+        return comm_str
+
+s = Solution()
+print s.longestCommonPrefix(["geeksforgeeks", "geeks", "geek", "geezer"])
+print s.longestCommonPrefix(["flower","flow","flight"])
