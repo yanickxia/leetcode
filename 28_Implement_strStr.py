54659c288fb2960bf0b873d02229fab3a6452b1e
yann <me.yan.xia@qq.com>
2017-08-02 23:25:03 +0800

    Add 28

---
+++ 28_Implement_strStr.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if not needle:
+            return 0
+        for i in xrange(len(haystack) - len(needle) + 1):
+            if haystack[i] == needle[0]:
+                j = 1
+                while j < len(needle) and haystack[i+j] == needle[j]:
+                    j += 1
+                if j == len(needle):
+                    return i
+        return -1
