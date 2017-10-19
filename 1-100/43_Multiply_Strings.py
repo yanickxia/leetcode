36cd98cb01fa98c4159f0e2610aebaa0c5fcfe43
yann <me.yan.xia@qq.com>
2017-10-19 12:12:33 +0800

    add 43

---
+++ 1-100/43_Multiply_Strings.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num3 = [""] * (len(num1) + len(num2) + 1)
+
+        return num1 * num2
+
+    @staticmethod
+    def multiplyN(num1, n):
+
+
+s = Solution()
+s.multiply("2", "3")
