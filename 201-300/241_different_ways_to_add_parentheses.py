3dd013b0142e73d879d8ec43ba59716028e29433
yann <me.yan.xia@qq.com>
2018-03-30 22:41:44 +0800

    mark

--- 201-300/241_different_ways_to_add_parentheses.py
+++ 201-300/241_different_ways_to_add_parentheses.py
@@ -6,47 +6,64 @@ class Solution(object):
         :type input: str
         :rtype: List[int]
         """
-        input_list = ["(" + input + ")"]
-        real_rs_list = []
-        real_rs_list.extend(input_list)
+        x = self.to_array(input)
 
-        for x in input_list:
-            x_list = list(x)
-            for i in range(1, len(x_list)):
-                if x_list[i].isdigit():
-                    an_list = list(x_list)
-                    an_list.insert(i, '(')
-                    new_x = ''.join(an_list)
-                    new_rs = Solution.insert_a_right_pathness(new_x)
-                    real_rs_list.extend(new_rs)
-                    input_list = new_rs
-        new_real_rs_list = []
-        for x in real_rs_list:
-            is_need = True
-            for i in range(0, len(x) - 2):
-                if x[i] == '(' and x[i + 2] == ')':
-                    is_need = False
+        return self.compute_reminder_array(x)
 
-            if is_need:
-                new_real_rs_list.append(x)
+    def calc_str(self, a):
+        rs, i = int(a[0]), 1
+        while i < len(a):
+            if a[i] == '+':
+                rs += int(a[i + 1])
+            elif a[i] == '-':
+                rs -= int(a[i + 1])
+            elif a[i] == '*':
+                rs *= int(a[i + 1])
+            i += 2
 
-        real_rs_list = [eval(item) for item in new_real_rs_list]
+        return rs
 
-        return real_rs_list
+    def opeartor_a_rs(self, a, opt, rs):
+        if opt == '*':
+            return map(lambda x: int(x) * int(a), rs)
+        elif opt == '+':
+            return map(lambda x: int(x) * int(a), rs)
+        else:
+            return map(lambda x: int(a) - int(x), rs)
 
-    @staticmethod
-    def insert_a_right_pathness(str_n):
-        a_list = list(str_n)
+    def compute_reminder_array(self, reminder):
         rs = []
-        for x in range(0, len(a_list)):
-            if a_list[x].isdigit():
-                if x > 1 and a_list[x - 2] == '(':
-                    continue
-                t_list = list(a_list)
-                t_list.insert(x + 1, ')')
-                rs.append(''.join(t_list))
-        return rs[0:len(rs) - 1]
+        if len(reminder) == 1:
+            return [int(reminder[0])]
+        for i in range(0, len(reminder), 2):
+            a = reminder[0: i + 1]
+            if len(a) == len(reminder):
+                # rs.extend([self.calc_str(a)])
+                break
+            opt = reminder[i + 1]
+            rs.extend(self.opeartor_a_rs(self.calc_str(a), opt, self.compute_reminder_array(reminder[i + 2:])))
+        return rs
+
+    def to_array(self, input):
+        a_input = list(input)
+        operator = []
+        for i in range(0, len(a_input)):
+            if a_input[i] in ('+', '-', '*'):
+                operator.append(a_input[i])
+                a_input[i] = ','
+
+        x = ''.join(a_input).split(',')
+        a_input = [x[0]]
+        i, j = 0, 1
+        while i < len(operator) and j < len(x):
+            a_input.append(operator[i])
+            a_input.append(x[j])
+            i += 1
+            j += 1
+        return a_input
 
 
 s = Solution()
+print(s.diffWaysToCompute("1-1"))
+print(s.diffWaysToCompute("2-1-1"))
 print(s.diffWaysToCompute("2*3-4*5"))
