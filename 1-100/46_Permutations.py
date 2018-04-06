35a1d862b6b48dea7ef3bf6945a4c3ca5756d69a
yann <me.yan.xia@qq.com>
2018-04-06 22:43:32 +0800

    p46/47

--- 1-100/46_Permutations.py
+++ 1-100/46_Permutations.py
@@ -1,1 +1,25 @@
 # -*- coding:utf-8 -*-
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) == 1:
+            return [[nums[0]]]
+        permute_rs = []
+        for i in range(0, len(nums)):
+            new_nums = list(nums)
+            del new_nums[i]
+            rs = self.permute(new_nums)
+            for x in rs:
+                t = [nums[i]]
+                t.extend(x)
+                permute_rs.append(t)
+
+        return permute_rs
+
+
+s = Solution()
+print(s.permute([1, 2, 3]))
--- 1-100/47_Permutations_II.py
+++ 1-100/47_Permutations_II.py
@@ -1,1 +1,25 @@
 # -*- coding:utf-8 -*-
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) == 1:
+            return [[nums[0]]]
+        permute_rs = []
+        for i in set(nums):
+            new_nums = list(nums)
+            new_nums.remove(i)
+            rs = self.permuteUnique(new_nums)
+            for x in rs:
+                t = [i]
+                t.extend(x)
+                permute_rs.append(t)
+
+        return permute_rs
+
+
+s = Solution()
+print(s.permuteUnique([1, 1, 2]))
