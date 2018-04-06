c0e73dded101463d40d128f8f27bde35c62330a2
yann <me.yan.xia@qq.com>
2018-04-06 16:47:54 +0800

    Add prolem 39 & 40

--- 1-100/40_Combination_Sum_II.py
+++ 1-100/40_Combination_Sum_II.py
@@ -1,13 +1,12 @@
 # -*- coding:utf-8 -*-
 
 class Solution(object):
-    def combinationSum(self, candidates, target):
+    def combinationSum2(self, candidates, target):
         """
         :type candidates: List[int]
         :type target: int
         :rtype: List[List[int]]
         """
-
         rs = []
         if target in candidates:
             rs.append([target])
@@ -16,9 +15,10 @@ class Solution(object):
         if len(less_than_target) == 0:
             return rs
 
-        for x in less_than_target:
+        for x in set(less_than_target):
             less_than_target_and_bigger_than_x = [y for y in less_than_target if y >= x]
-            xr = self.combinationSum(less_than_target_and_bigger_than_x, target - x)
+            less_than_target_and_bigger_than_x.remove(x)
+            xr = self.combinationSum2(less_than_target_and_bigger_than_x, target - x)
             for one_xr in xr:
                 temp = [x]
                 temp.extend(one_xr)
@@ -28,5 +28,4 @@ class Solution(object):
 
 
 s = Solution()
-print(s.combinationSum([1], 1))
-print(s.combinationSum([2, 3, 6, 7], 7))
+print(s.combinationSum2([10, 1, 2, 7, 6, 1, 5], 8))
