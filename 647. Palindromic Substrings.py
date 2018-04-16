9e436f7fafdc39e512fa8b2203ed60066449dd20
yann <me.yan.xia@qq.com>
2018-04-16 22:42:41 +0800

    Add problem 647
    i hate dp again

--- 647. Palindromic Substrings.py
+++ 647. Palindromic Substrings.py
@@ -6,8 +6,18 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-
+        rs = [[True] * len(s)] + [[False for x in range(len(s))] for x in range((len(s) - 1))]
+        count = len(s)
+        for i in range(1, len(s)):
+            for j in range(i, len(s)):
+                a = s[j]
+                b = s[j - i]
+                rs[i][j] = a == b and (rs[i - 2][j - 1] or i < 2)
+                if rs[i][j]:
+                    count += 1
+        return count
 
 
 s = Solution()
-print(s.countSubstrings("abcb"))
+print(s.countSubstrings("aaa"))
+print(s.countSubstrings("abc"))
