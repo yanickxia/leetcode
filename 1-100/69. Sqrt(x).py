a02bb8f22469feab32838b9c3f028f80710ef6c6
yann <me.yan.xia@qq.com>
2018-04-27 17:11:07 +0800

    Add 69

--- 1-100/69. Sqrt(x).py
+++ 1-100/69. Sqrt(x).py
@@ -1,1 +1,29 @@
 # -*- coding:utf-8 -*-
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        # 二分查找
+        if x < 2:
+            return x
+        low, high = 1, x / 2
+        last = 0
+        while low <= high:
+            mid = (low + high) / 2
+            if mid * mid > x:
+                high = mid - 1
+            elif mid * mid < x:
+                low = mid + 1
+                last = mid
+            else:
+                return mid
+        return last
+
+
+s = Solution()
+print(s.mySqrt(1))
+print(s.mySqrt(4))
+print(s.mySqrt(8))
+print(s.mySqrt(6))
