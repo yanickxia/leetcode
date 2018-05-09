5b0fda2ccdecd0d4bb86421d2d103da4fe644e65
yann <me.yan.xia@qq.com>
2018-05-09 22:39:36 +0800

    Add 198

--- 101-200/198. House Robber.py
+++ 101-200/198. House Robber.py
@@ -1,1 +1,24 @@
 # -*- coding:utf-8 -*-
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxS = [0] * len(nums)
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        maxS[0] = nums[0]
+        maxS[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            maxS[i] = max(nums[i] + maxS[i - 2], maxS[i - 1])
+
+        return maxS[-1]
+
+
+s = Solution()
+print(s.rob([2, 7, 9, 3, 1]))
