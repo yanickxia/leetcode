451a9a3600c57c52293edf9e15b1cdc7cea3e5a6
yann <me.yan.xia@qq.com>
2018-08-18 15:29:50 +0800

    Add no.51 & no.52

---
+++ 1-100/52-N-Queens-2.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+
+        def check(k, j):
+            for i in range(k):
+                if board[i] == j or abs(board[i] - j) == abs(k - i):
+                    return False
+            return True
+
+        def dfs(depth, valuelist):
+            if depth == n:
+                res.append(valuelist)
+                return
+            else:
+                for i in range(n):
+                    if check(depth, i):
+                        board[depth] = i
+                        s = '.' * n
+                        dfs(depth + 1, valuelist + [s[:i] + 'Q' + s[i + 1:]])
+
+        res = []
+        board = [-1 for i in range(n)]
+        dfs(0, [])
+        return len(res)
