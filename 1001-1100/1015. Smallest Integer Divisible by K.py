af21b81f574594d0d76d4b73e95dc8cb1da0b725
yanick.xia <yann.xia@daocloud.io>
2020-08-27 17:49:01 +0800

    Add 1015

---
+++ 1001-1100/1015. Smallest Integer Divisible by K.py
@@ -0,0 +1,9 @@
+class Solution:
+    def smallestRepunitDivByK(self, K: int) -> int:
+        if K % 2== 0 or K % 5 ==0:
+            return -1
+        else:
+            ans = 1
+            while ans % K != 0:
+                ans = ans * 10 + 1
+            return len(str(ans))
