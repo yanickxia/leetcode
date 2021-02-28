7808d0688fafd792502b94a4ed115084d2b2b720
yanick <yanick.xia@gmail.com>
2021-02-28 19:35:15 +0800

    Add more

--- 1401-1500/1402. Reducing Dishes.py
+++ 1401-1500/1402. Reducing Dishes.py
@@ -2,14 +2,32 @@ from typing import List
 
 
 ###
-
+#
 # i,j -> Max Satisfaction At Rang(i,j)
-# F(i,j) = Max(F(x-1), F(x-1) + (x * S[x-1]))
-# F(1) = Max(0, 1*S[0])
-# F(2) = Max(F(1), F(1) + 2 * S[1]), F()
+# F(i,j) -> Max( Max(F(i,j-1), F(i,j-1)+ S[j-1]*j), Max(F(i+1,j), F(i+1,j) + S[i] ))
+#        -> Max(0,S[i-1]*i) IF i == j
 ###
 
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        max_satisfaction = [0] * (len(satisfaction) + 1)
+        i, j, n = 1, 1, len(satisfaction) + 1
+        max_satisfaction = [[0 for j in range(n)] for i in range(n)]
+        for i in range(1, n):
+            for j in range(1, n):
+                if i == j:
+                    max_satisfaction[i][i] = max(0, satisfaction[i - 1])
 
+        for k in range(1, n):
+            for i in range(1, n - k):
+                for j in range(i + k, n):
+                    max_satisfaction[i][j] = max(
+                        max(max_satisfaction[i][j - 1], max_satisfaction[i][j - 1] + ((j - i) * satisfaction[j - 1])),
+                        max(max_satisfaction[i + 1][j], max_satisfaction[i + 1][j] + satisfaction[i])
+                    )
+
+        print(max_satisfaction)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.maxSatisfaction([-1, -8, 0, 5, -9]))
